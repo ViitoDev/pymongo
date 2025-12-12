8d029d0f4c4b07ef7631e09e46b38df9e54c88e6
ViitoDev <joaovictorpiresdev@gmail.com>
2025-12-11 21:38:12 -0300

    FEAT - find data

---
+++ find_data.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['nobel']
+prizes = db['prizes']
+laureates = db['laureates']
+
+print(db.laureates.count_documents({'gender': 'female'}))
+print(db.laureates.count_documents({'gender': 'male'}))
+
+print(db.laureates.count_documents({'diedCountry': 'France'}))
+
+filter_doc =  {
+    'diedCountry': 'France',
+    'gender': 'female',
+    'bornCity': 'Warsaw',
+}
+
+print(db.laureates.count_documents(filter_doc))
+print(db.laureates.find_one(filter_doc))
+
+filter_in = db.laureates.count_documents(
+    {
+        'diedCountry': {
+            '$in':['France', 'USA']
+        }
+    }
+)
+
+print(filter_in)
+
+filter_ne = db.laureates.count_documents(
+    {
+        'diedCountry': {
+            '$ne': 'USA'
+        }
+    }
+)
+
+print(filter_ne)
